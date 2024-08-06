6af6b5ce3b335f5f0bf3de76193caabc6570f237
blakbox23 <boudii30@gmail.com>
2024-08-06 17:57:30 +0300

    initial commit

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  function handleDelete(id) {
+    
+    const remainingQuestions = questions.filter(
+      (question) => question.id !== id
+    );
+    setQuestions(remainingQuestions);
+
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    });
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
